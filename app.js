c61854d87eb520eba1c50b4ec02e103d29a353d2
taem <taem@taemui-MacBookAir.local>
2025-09-01 11:50:59 +0900

    express setting

---
+++ app.js
@@ -0,0 +1,15 @@
+// express 부트스트랩 파일
+import express from "express";
+import indexRouter from "./routes/index.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use("/", indexRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
