9a080afd29e6b1ef689899dfb31ea1d5d5bed2ed
taem <taem@taemui-MacBookAir.local>
2025-09-02 11:41:09 +0900

    config .env

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ import express from "express";
 import indexRouter from "./routes/index-router.js";
 import productsRouter from "./routes/products-router.js";
 import routeLog from "./middleware/route-log.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(express.json());
