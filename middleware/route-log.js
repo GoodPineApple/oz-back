30810b7ce14161133825ce32e7d7d01bca775e34
taem <taem@taemui-MacBookAir.local>
2025-09-02 11:15:10 +0900

    moment in route-log

--- middleware/route-log.js
+++ middleware/route-log.js
@@ -1,7 +1,8 @@
 // 라우트 로깅 미들웨어
+import moment from "moment";
+
 const routeLog = (req, res, next) => {
-    const now = new Date();
-    const formattedDate = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+    const formattedDate = moment().format("YYYY-MM-DD HH:mm:ss");
     console.log(`${formattedDate} - ${req.method} ${req.baseUrl}${req.path}`);
     next();
 };
