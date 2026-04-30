3c5ebbd0817bb9c3d18564102d22074b5317dfcc
GoodPineApple <taemin3000@naver.com>
2026-04-30 14:40:05 +0900

    base FileManager

--- base/FileManager.js
+++ base/FileManager.js
@@ -1,7 +1,13 @@
 const fs = require("fs");
+const path = require("path");
 
-function createFile() {
-  fs.writeFileSync("hello.txt", "Hello, World!");
+const filePath = path.join(__dirname, "..", "files");
+console.log(filePath); // /Users/oz/Documents/oz-back/files
+
+function createFile(fileName, content) {
+  console.log(fileName);
+  console.log(content);
+  fs.writeFileSync(path.join(filePath, fileName), content);
 }
 
 module.exports = { createFile };
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { createFile } = require("./base/FileManager");
 const { main } = require("./base/sum");
 
-// createFile();
-main();
+createFile("hello.txt", "Hello, World! Good Morning!");
+createFile("hello2.txt", "Hello, World!2 Good Morning!");
+createFile("hello3.txt", "Hello, World!3 Good Morning!");
+// main();
