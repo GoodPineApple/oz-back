a0fdecf3309a4d664a1dbad65526b280af096cd6
GoodPineApple <taemin3000@naver.com>
2026-05-04 11:14:15 +0900

    log manager

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const { testCreateFiles } = require("./base/FileManager");
 const { main } = require("./base/sum");
+const { logMessage } = require("./base/LogManager");
 
 // main();
-testCreateFiles();
+// testCreateFiles();
+logMessage("이건 메세지5");
+logMessage("이건 메세지6");
+logMessage("이건 메세지7");
+logMessage("이건 메세지8");
+logMessage("이건 메세지9");
+logMessage("이건 메세지10");
