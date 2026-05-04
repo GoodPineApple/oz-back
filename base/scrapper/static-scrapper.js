9a4cbc7d13efd1592f2524dad90b3b97e74c5b53
GoodPineApple <taemin3000@naver.com>
2026-05-04 13:21:05 +0900

    axios test

--- base/scrapper/static-scrapper.js
+++ base/scrapper/static-scrapper.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const URL = "https://www.npmjs.com/package/puppeteer";
+
 async function crawl() {
-  const response = await axios.get("https://www.google.com");
+  const response = await axios.get(URL);
   console.log(response.data);
 }
 
