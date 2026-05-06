0235b82a4d352e7b93598e3df10d19548f72a0d2
GoodPineApple <taemin3000@naver.com>
2026-05-06 14:21:08 +0900

    user router

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 // express의 부트스트랩 파일
 import express from "express";
+import userRoute from "./routes/user-route.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static("public"));
 
+app.use("/users", userRoute);
+
 app.get("/", (req, res) => {
   const reqUrl = req.url;
   const reqMethod = req.method;
   console.log(reqUrl, reqMethod);
-  res.send("Hello World");
+  res.status(202).send("Hello World");
 });
 
 app.get("/posts", (req, res) => {
---
+++ routes/user-route.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+const router = express.Router();
+
+const users = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Jane" },
+  { id: 3, name: "Jim" },
+];
+
+// GET /users 데이터 조회
+router.get("/", (req, res) => {
+  res.json(users);
+});
+
+// GET /users/:id 데이터 조회
+router.get("/:id", (req, res) => {
+  const userId = req.params.id;
+  const user = users.find((user) => user.id === userId);
+  res.json(user);
+});
+
+export default router;
