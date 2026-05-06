263e9bc83c3de830ea7953710991e5a827df74b3
GoodPineApple <taemin3000@naver.com>
2026-05-06 13:15:57 +0900

    express get

--- app.js
+++ app.js
@@ -2,7 +2,21 @@
 import express from "express";
 
 const app = express();
+const PORT = 3000;
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/posts", (req, res) => {
+  const posts = [
+    { id: 1, title: "Hello World" },
+    { id: 2, title: "Hello World" },
+    { id: 3, title: "Hello World" },
+  ];
+  res.json(posts);
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
 });
